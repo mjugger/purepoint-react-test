316075b6544aa82b2af9f6eebbec46833520ab98
Mukhtar Jugger <xaiox20@gmail.com>
2018-06-17 23:22:45 -0400

    changed conf file name, updated server_url constant

--- app/global/global.constants.js
+++ app/global/global.constants.js
@@ -1,2 +1,3 @@
-export const SERVER_URL = 'http://localhost:3006';
+//export const SERVER_URL = 'http://localhost:3006';
+export const SERVER_URL = '';
 //export const SERVER_URL = 'http://url-to-external-server;
